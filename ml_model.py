5885eaa70a9f8ad0a060bea5e34fa23ed34ac0d4
Meraj20 <merajalam.ra@gmail.com>
2024-08-22 14:37:00 +0530

    added 3 new ml model

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,10 @@
 print("adding all ml related code here")
 
+def model1():
+    print("adding code for ML Model1")
+
+def model2():
+    print("adding code for ML Model2")
+    
+def model3():
+    print("adding code for ML Model3")
